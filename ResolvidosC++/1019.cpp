f5026253be05ba5d50bfdc79a561be1dd3278680
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-24 12:14:27 -0300

    Update 1019.cpp

--- ResolvidosC++/1019.cpp
+++ ResolvidosC++/1019.cpp
@@ -1,1 +1,25 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+
+    int n,h=0,m=0,s=0;
+    cin>>n;
+    if(n>=3600){
+        h=n/3600;
+        n=n%3600;
+    }
+    if(n>=60){
+        m=n/60;
+        s=n%60;   
+    }
+    if(n<60){
+        s=n;
+    }
+
+    cout<<h<<":"<<m<<":"<<s<<endl;
+
+
+    return 0;
+}
