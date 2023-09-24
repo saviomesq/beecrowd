397dba0df2abdea9bdd70ece93037069531dd55a
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-24 12:08:15 -0300

    Create 1017.cpp
    1017 resolvido em c++

---
+++ 1017.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int t,v;
+    double res;
+
+    cin>>t>>v;
+    res=t*v/12.0;
+    cout<<fixed<<setprecision(3)<<res<<endl;
+
+
+
+    return 0;
+}
