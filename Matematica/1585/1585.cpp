d4453aaba65fcaaf4e674c53f11159b9ddfdadc2
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-26 19:30:05 -0300

    Create 1585.cpp

---
+++ Matematica/1585/1585.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,a,b,c;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        c=(a*b)/2;
+        cout<<c<<" cm2"<<endl;
+    }
+
+
+    return 0;
+}
