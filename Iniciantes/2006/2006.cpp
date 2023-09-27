f6cabd75474604a4f8839f0fb4388eb698f5d705
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-27 20:50:18 -0300

    Create 2006.cpp

---
+++ Iniciantes/2006/2006.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int cha,a,b,c,d,e,cont=0;
+    cin>>cha;
+    cin>>a>>b>>c>>d>>e;
+    if(a==cha){
+        cont++;
+    }
+    if(b==cha){
+        cont++;
+    }
+    if(c==cha){
+        cont++;
+    }
+    if(d==cha){
+        cont++;
+    }
+    if(e==cha){
+        cont++;
+    }
+    cout<<cont<<endl;
+
+    return 0;
+}
