df53c1536716ca0ada521874dd8236456a2ad949
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-27 20:48:56 -0300

    Create 1026.cpp

---
+++ Ad-Hoc/1026/1026.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ 
+int
+main ()
+{
+  
+ 
+unsigned int a, b, c;
+  
+ 
+while (cin >> a >> b)
+    {
+      
+c = a ^ b;
+      
+cout << c << endl;
+    
+}
+  
+ 
+ 
+ 
+ 
+return 0;
+
+}
