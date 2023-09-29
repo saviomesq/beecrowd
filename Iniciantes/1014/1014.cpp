f99fa09a34c34b2edf47814344a55866bc0472b7
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-29 12:20:10 -0300

    Create 1014.cpp

---
+++ Iniciantes/1014/1014.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+#include<bits/stdc++.h>    
+
+int main(){
+
+   int x;
+   double b,media;
+
+   cin>>x>>b;
+   media= x/b;
+   cout<<fixed;
+   cout<<setprecision(3)<<media<<" km/l"<<endl; 
+
+}
