cb8862118152f4965f0e196ca38287e0a9948cec
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-29 12:18:57 -0300

    Create 1011.cpp

---
+++ Iniciantes/1011/1011.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include<bits/stdc++.h>    
+
+int main(){
+
+    double r;
+    double volume;
+    
+    cin>>r;
+    r= pow(r,3);
+    cout<<fixed;
+    cout<<setprecision(3)<<"VOLUME = "<<(4/3.0)*3.14159*r<<endl;
+
+    return 0;
+
+}
