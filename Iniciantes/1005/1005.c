1305d8346b7227f188698af3bf3604512ebf40ac
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-29 12:21:32 -0300

    Create 1005.c

---
+++ Iniciantes/1005/1005.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+ int main () {
+      double a,b,media;
+      scanf("%lf",&a);
+      scanf("%lf",&b);  
+
+   media =((3.5*a)+(7.5*b))/11;
+    printf("MEDIA = %.5f\n",media);
+     return 0 ;
+     }
