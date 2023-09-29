9b7298e35a4c793e5c75b858f6ebccd3caa18e8e
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-29 12:16:11 -0300

    Create 1003.cpp

---
+++ Iniciantes/1003/1003.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ int valor1, valor2, soma;
+ scanf("%d",&valor1);
+ scanf("%d",&valor2);
+ soma= valor1+valor2;
+ printf("SOMA = %d\n", soma);
+
+    return 0;
+}
