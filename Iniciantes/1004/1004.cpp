952e67a71c7f4c9a8010d3bc681dc7aea1695a3d
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-09-29 12:16:30 -0300

    Create 1004.cpp

---
+++ Iniciantes/1004/1004.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int n1,n2, prod;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    prod=n1*n2;
+    printf("PROD = %d\n", prod);
+
+    return 0;
+}
