508d8e8f6a6bd9114d64c384a11ccd8a267eb115
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-10-01 22:57:21 -0300

    Create 1114.cpp

---
+++ Iniciantes/1114/1114.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int x=0;
+    
+    while(x!=2002){
+        cin>>x;
+        if(x==2002){
+            cout<<"Acesso Permitido"<<endl;
+        }
+       else
+         cout<<"Senha Invalida"<<endl;
+    }
+
+
+    return 0;
+}
