ef7edbe5765daafe63a61a87acbf37d1a2721782
Sávio Mesquita <99457301+saviomesq@users.noreply.github.com>
2023-10-04 18:56:07 -0300

    Create 1096.c

---
+++ Iniciantes/1096/1096.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{  
+  int i=1,j=7;
+  do{
+    printf("I=%d J=%d\n",i,j);
+    printf("I=%d J=%d\n",i,j-1);
+    printf("I=%d J=%d\n",i,j-2);
+    i=i+2;
+    
+  }while(i!=11);
+}
